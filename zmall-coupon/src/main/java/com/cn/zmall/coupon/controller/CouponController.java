5850e7e157e1039366c8fed6a9fceca77c485516
chennan <905394587@qq.com>
2023-08-17 10:58:33 +0800

    云配置

--- zmall-coupon/src/main/java/com/cn/zmall/coupon/controller/CouponController.java
+++ zmall-coupon/src/main/java/com/cn/zmall/coupon/controller/CouponController.java
@@ -1,20 +1,14 @@
 package com.cn.zmall.coupon.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cn.zmall.coupon.entity.CouponEntity;
-import com.cn.zmall.coupon.service.CouponService;
 import com.cn.zmall.common.utils.PageUtils;
 import com.cn.zmall.common.utils.R;
+import com.cn.zmall.coupon.entity.CouponEntity;
+import com.cn.zmall.coupon.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Map;
 
 
 /**
@@ -30,11 +24,12 @@ public class CouponController {
     @Autowired
     private CouponService couponService;
 
+
     /**
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = couponService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -45,8 +40,8 @@ public class CouponController {
      * 信息
      */
     @RequestMapping("/info/{id}")
-    public R info(@PathVariable("id") Long id){
-		CouponEntity coupon = couponService.getById(id);
+    public R info(@PathVariable("id") Long id) {
+        CouponEntity coupon = couponService.getById(id);
 
         return R.ok().put("coupon", coupon);
     }
@@ -55,8 +50,8 @@ public class CouponController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody CouponEntity coupon){
-		couponService.save(coupon);
+    public R save(@RequestBody CouponEntity coupon) {
+        couponService.save(coupon);
 
         return R.ok();
     }
@@ -65,8 +60,8 @@ public class CouponController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody CouponEntity coupon){
-		couponService.updateById(coupon);
+    public R update(@RequestBody CouponEntity coupon) {
+        couponService.updateById(coupon);
 
         return R.ok();
     }
@@ -75,8 +70,8 @@ public class CouponController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
-		couponService.removeByIds(Arrays.asList(ids));
+    public R delete(@RequestBody Long[] ids) {
+        couponService.removeByIds(Arrays.asList(ids));
 
         return R.ok();
     }
--- zmall-product/src/main/java/com/cn/zmall/product/service/impl/CategoryServiceImpl.java
+++ zmall-product/src/main/java/com/cn/zmall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,16 @@
 package com.cn.zmall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cn.zmall.common.utils.PageUtils;
 import com.cn.zmall.common.utils.Query;
-
 import com.cn.zmall.product.dao.CategoryDao;
 import com.cn.zmall.product.entity.CategoryEntity;
 import com.cn.zmall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("categoryService")
