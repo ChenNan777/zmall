cb5c0cade19fb9211c3ca3c20fcbc2f228be80a9
chennan <905394587@qq.com>
2023-09-19 19:53:02 +0800

    oss

--- zmall-product/src/test/java/com/cn/zmall/product/ZmallProductApplicationTests.java
+++ zmall-product/src/test/java/com/cn/zmall/product/ZmallProductApplicationTests.java
@@ -22,48 +22,5 @@ class ZmallProductApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    void upLoadTest() throws com.aliyuncs.exceptions.ClientException {
-
-        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
-        //accessKeyId
-        String accessKeyId = "LTAI5tRfjzgvLMMQn5qz2498";
-        //secretAccessKey
-        String secretAccessKey = "VfVY6FCwDNxPuN9SQjv2l6Z100vc22";
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "zmall-cn";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = "test.jpg";
-        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-        String filePath = "C:\\Users\\90539\\Desktop\\pics\\0d40c24b264aa511.jpg";
-
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, secretAccessKey);
-
-        try {
-            // 创建PutObjectRequest对象。
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new File(filePath));
-            // 创建PutObject请求。
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with OSS, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
-        } finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
-    }
 
 }
